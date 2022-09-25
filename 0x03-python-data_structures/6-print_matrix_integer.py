127c516376657ba45c7716f3ecd8526541243ba7
Ephraim <ephraimigbinosa@gmail.com>
2022-09-25 14:50:50 +0100

    print 2d array

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        i = 0
         for col in row:
-            if i == len(row) - 1:
+            if col == row[-1]:
                 print("{:d}".format(col))
             else:
                 print("{:d}".format(col), end=" ")
-            i += 1
