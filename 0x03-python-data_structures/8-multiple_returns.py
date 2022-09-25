0986a86e00a8fa78ce20ebbc295c17be50d7549f
Ephraim <ephraimigbinosa@gmail.com>
2022-09-25 15:18:42 +0100

    return a tuple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,8 +4,3 @@ def multiple_returns(sentence):
     if n == 0:
         return (0, None)
     return (n, sentence[0])
-
-
-sentence = "At school, I learnt C!"
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))
