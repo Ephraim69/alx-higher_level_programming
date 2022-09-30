b0f58ada582d708fca9d416bf809a942d4ebd539
Ephraim <ephraimigbinosa@gmail.com>
2022-09-30 19:16:07 +0100

    print the key of the biggest value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,11 +3,8 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
 
-    max = list(a_dictionary.values())[0]
-    for value in a_dictionary.values():
-        if value > max:
-            max = value
+    max_value = max(a_dictionary.values())
 
     for key, value in a_dictionary.items():
-        if value == max:
+        if value == max_value:
             return key
