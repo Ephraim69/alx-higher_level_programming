6e1cc075068d7f9f811e02460900f22e1bdfd678
Ephraim <ephraimigbinosa@gmail.com>
2022-10-19 13:55:55 +0100

    first name

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 class LockedClass:
-    def __setattr__(self, key, value):
-        if key != "first_name":
-            raise AttributeError(
-                f"'LockedClass' object has no attribute {key}")
-        object.__setattr__(self, key, value)
+    __slots__ = ['first_name']
+
+    def __init__(self, first_name=""):
+        self.first_name = first_name
