f9f1ca56114252c1a4c580d29d85a3ed7ae6d5d3
Ephraim <ephraimigbinosa@gmail.com>
2022-10-25 20:15:20 +0100

    a class MyList that inherits from list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+class MyList(list):
+    """_A class that inherits from list
+
+    Args:
+        list (_type_): _description_
+    """
+    def print_sorted(self):
+        """Prints the sorted list
+        """
+        new_list = self[:]
+        new_list.sort()
+        print(new_list)
