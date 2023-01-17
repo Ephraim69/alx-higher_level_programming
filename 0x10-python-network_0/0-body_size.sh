16838509e3db3c014e121e4e564f72bab1760053
Ephraim <ephraimigbinosa@gmail.com>
2023-01-17 23:13:28 +0100

    reduce length to 3 lines

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # Bash script that displays size of a HTTP response body
-if [ $# -eq 1 ];
-then
-    curl -s -w '%{size_download}\n' -o /dev/null "$1"
-fi
+curl -s -w '%{size_download}\n' -o /dev/null "$1"
